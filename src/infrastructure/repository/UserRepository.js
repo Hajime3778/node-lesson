432a0ad91b7409e5365ae8e7c21a55a7b602a632
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-12-22 18:31:15 +0900

    配列形式になってたので修正

--- src/infrastructure/repository/UserRepository.js
+++ src/infrastructure/repository/UserRepository.js
@@ -16,7 +16,7 @@ class UserRepository{
     const sql = 'select * from users where ?';
     return new Promise((resolve, reject) => {
       this.connection.query(sql, {id: id}, (err, results) => {
-        return err ? reject(err.message) : resolve(results);
+        return err ? reject(err.message) : resolve(results[0]);
       });
     });
   }
